03e9523fcd78e981d389963cb5b700687592d7b5
lef73 <fes@gmx.net>
2011-05-05 08:58:11 -0700

    Edited APISecrets.lua via GitHub

--- APISecrets.lua
+++ APISecrets.lua
@@ -3,7 +3,7 @@
 -- KEEP THIS FILE IN A SECURE PLACE
 --------------------------------------------------------------------------------------------------
 
-APIKEY = '9170c87b0000274Fbd18c1d17a55322b'
-APISECRET = 'ca402308c40e42d9'
+APIKEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+APISECRET = 'xxxxxxxxxxxxxxxx'
 APIURL = 'http://api.ipernity.com/api'
 AUTHURL = 'http://www.ipernity.com/apps/authorize'
